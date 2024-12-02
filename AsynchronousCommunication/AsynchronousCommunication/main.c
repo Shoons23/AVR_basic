a8ac07fc489736217b77ae7c7268df9faec6bfcd
Shoons23 <jaehoon9723@gmail.com>
2024-12-02 16:57:01 +0900

    22. USART
    1. UART is used to asynchronous communication.

---
+++ AsynchronousCommunication/AsynchronousCommunication/main.c
@@ -0,0 +1,19 @@
+/*
+ * AsynchronousCommunication.c
+ *
+ * Created: 2024-12-02 오전 10:20:04
+ * Author : tshoon
+ */ 
+
+#include <avr/io.h>
+#include "board.h"
+#include "fnd.h"
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ AsynchronousCommunication/AsynchronousCommunication/uart.h
@@ -0,0 +1,19 @@
+﻿/*
+ * uart.h
+ *
+ * Created: 2024-12-02 오후 3:47:33
+ *  Author: tshoon
+ */ 
+
+
+#ifndef UART_H_
+#define UART_H_
+
+void uart_init(void);
+int8_t uart_getch(void);
+void uart_putch(int8_t ch);
+void uart_puts(int8_t* str);
+void num2ascii(int8_t ch);
+
+
+#endif /* UART_H_ */
